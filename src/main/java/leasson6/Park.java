e14debb72031e1eb687f65cd7863951ac3b9585a
Dom <andreu_semin@mail.ru>
2024-06-30 12:22:48 +0300

    Исправленное ДЗ6

--- src/main/java/leasson6/Park.java
+++ src/main/java/leasson6/Park.java
@@ -1,47 +1,51 @@
 package leasson6;
+
+import java.util.ArrayList;
+import java.util.List;
+
 //3. Создать класс Park с внутренним классом,
 // с помощью объектов которого можно хранить информацию об аттракционах, времени их работы и стоимости.
 public class Park {
     private String name;
+    private List<Attraction> attractions;
 
 
     public Park(String name) {
         this.name = name;
-
+        this.attractions = new ArrayList<>();
     }
 
-    public class Attractions {
+    public void addAtttraction(String info, String wortTime, int price) {
+        Attraction attraction = new Attraction(info, wortTime, price);
+        attractions.add(attraction);
+    }
+
+    public void printInfo() {
+        System.out.println(name);
+        for (Attraction attraction : attractions) {
+            System.out.println(attractions);
+        }
+    }
+
+
+    static class Attraction {
+        private String name;
         private String timeWork;
-        private String info;
-        private int prise;
+        private int price;
 
-        public Attractions(String timeWork, String info, int prise) {
+        private Attraction(String name, String timeWork, int price) {
+            this.name = name;
             this.timeWork = timeWork;
-            this.info = info;
-            this.prise = prise;
-        }
-        public void getAttractions(String timeWork, String info, int prise){
-            Attractions attractions = new Attractions(timeWork, info, prise);
+            this.price = price;
         }
 
-    }
-    public String getName(String name){
-        return name;
-    }
-    public String toString(){
-        return String.format("Парк: %s",name);
-    }
-    public void print(){
-        System.out.println(this);
-    }
-    public static void main(String[] args){
-        Park park = new Park("Аркадия");
-        Park.Attractions attractions = park.new Attractions("10:00 - 17:00", "Колесо", 200 );
-        park.print();
-        System.out.println("Название атракциона " + attractions.info);
-        System.out.println("Время работы " + attractions.timeWork);
-        System.out.println("Cтоимость " + attractions.prise);
-    }
+        @Override
 
+        public String toString() {
+            return "Attraction name: " + name + "\n" +
+                    "Time Work : " + timeWork + "\n" +
+                    "Price : " + price + "\n";
+        }
 
+    }
 }
